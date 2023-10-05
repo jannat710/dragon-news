e615894787b6944c98633a0a7dd49301477f45a6
e <jannatul0040@gmail.com>
2023-10-05 16:01:54 +0600

    added right navigation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,26 @@
 import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
+import BreakingNews from "./BreakingNews"
 
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <BreakingNews></BreakingNews>
             <Navbar></Navbar>
-            <h2 className="text-3xl font-poppins font-bold">This is home</h2>
+            
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div className="border ">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="border md:col-span-2">
+                <h2 className="text-4xl font-poppins font-bold">News coming soon...</h2>
+                </div>
+                <div className="border "><RightSideNav></RightSideNav></div>
+            </div>
         </div>
     );
 };
--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,44 @@
-
+import {FaGoogle,FaGithub, FaInstagram,FaFacebook, FaTwitter } from 'react-icons/fa';
+import qZone1 from '../../../assets/qZone1.png';
+import qZone2 from '../../../assets/qZone2.png';
+import qZone3 from '../../../assets/qZone3.png';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2 className="text-2xl">Right Side</h2>
+            <div className="p-4 space-y-3 mb-6">
+                <h2 className="text-3xl">Login with</h2>
+                <button className="btn btn-outline w-full normal-case">
+                    <FaGoogle></FaGoogle>
+                    Login with Google
+                </button>
+                <button className="btn btn-outline w-full normal-case">
+                    <FaGithub></FaGithub>
+                    Login with Github
+                </button>
+            </div>
+            <div className="p-4 mb-6">
+                <h2 className="text-3xl mb-4">Find Us on</h2>
+                <a className='flex p-4 text-lg items-center border  rounded-t-lg' href="">
+                    <FaFacebook className='mr-3'></FaFacebook>
+                    <span>Facebook</span>
+                </a>
+                <a className='flex p-4 text-lg items-center border-x' href="">
+                    <FaTwitter className='mr-3'></FaTwitter>
+                    <span>Twitter</span>
+                </a>
+                <a className='flex p-4 text-lg items-center border  rounded-b-lg' href="">
+                    <FaInstagram className='mr-3'></FaInstagram>
+                    <span>Instagram </span>
+                </a>
+            </div>
+            {/* q zone  */}
+            <div className='p-4 space-y-3 mb-6'>
+                <h2 className="text-3xl">Q Zone</h2>
+                <img src={qZone1} alt="" />
+                <img src={qZone2} alt="" />
+                <img src={qZone3} alt="" />
+            </div>
         </div>
     );
 };
